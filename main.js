9ca0e733de08dee40a74b304744626d7d981ffed
himcs <lik0x01@sp.noahgroup.com>
2021-04-08 14:17:31 +0800

    登录逻辑变更

--- main.js
+++ main.js
@@ -1,34 +1,47 @@
+md5 = require('js-md5');
+
+
 const login = require("./api/auth").login;
 const pause = require("./api/auth").pause;
 
 const Secrets = {
     username: process.env.LEISHEN_USERNAME,
-    password: process.env.LEISHEN_PASSWORD
+    password: md5(process.env.LEISHEN_PASSWORD)
 }
 
 
 function start(username, password) {
+    console.log('🌀雷神加速器暂停助手 开始运行-------')
     if (!username || !password) {
         console.log("Empty username or password");
         return;
     }
     const user = {
-        "username": username,
-        "password": password,
-        "user_type": "0",
-        "src_channel": "guanwang",
-        "lang": "zh_CN",
-        "region_code": 1
+        account_token: null,
+        country_code: 86,
+        lang: "zh_CN",
+        password: Secrets.password,
+        region_code: 1,
+        src_channel: "guanwang",
+        user_type: "0",
+        username: Secrets.username
     };
 
     login(user).then(res => {
         if (res.data.code == 0) {
             let account_token = res.data.data.login_info.account_token;
-            pause({"account_token": account_token, "lang": "zh_CN"}).then(res2 => {
-                console.log(res2);
+            pause({ "account_token": account_token, "lang": "zh_CN" }).then(res2 => {
+                console.log(res2.data.code + ':' + res2.data.msg);
+                console.log('🌀雷神加速器暂停助手 成功-------')
+
             })
+        } else {
+            console.log('🌀雷神加速器暂停助手 失败-------')
         }
+        console.log('🌀雷神加速器暂停助手 结束运行-------')
     })
+
+
 }
 
 
